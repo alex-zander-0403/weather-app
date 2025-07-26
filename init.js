57b1191328561c39f6359d37cfe280dfe6956372
alex-zander-0403 <alexzander0403@gmail.com>
2025-07-26 06:16:02 +0300

    geolocation

--- init.js
+++ init.js
@@ -2,10 +2,12 @@ import { switchTheme } from "./components/switchTheme.js";
 import { getGeoData } from "./src/api/geoData.js";
 import { getWeatherByForm } from "./components/inputForm.js";
 import { renderCurrentTime } from "./src/helpers/currentTime.js";
+import { geoLocation } from "./components/geolocation.js";
 
 export function initApp() {
   switchTheme();
   getGeoData();
   getWeatherByForm();
   renderCurrentTime();
+  geoLocation();
 }
